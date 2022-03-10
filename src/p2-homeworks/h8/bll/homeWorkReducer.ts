8ed5b57b5fe4cc4f83d4c9599fc3b33fdf74d753
Maks-KaNDeR <makskander1@gmail.com>
2022-03-10 16:20:18 +0300

    fix, hw8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -7,11 +7,13 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': {
             // need to fix
-            const newState = {...state}
-            return action.payload === 'up' 
-            ? newState.sort() 
-            : newState.reverse() 
-        }
+            const newState = [...state].sort((a,b) => {
+                if (a.name > b.name) return 1 
+                else if (a.name < b.name) return -1
+                else return 0 
+            })
+        return action.payload === 'up' ? newState : newState.reverse()
+    } 
         case 'check': {
             // need to fix
             
--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { MouseEvent, useState } from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
@@ -7,25 +7,49 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
+        setDate(new Date())
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const d = new Date()
+
+    const time = () => {
+        let h = d.getHours()
+        let m = d.getMinutes()
+        let s = d.getSeconds()
+
+        let hour = h < 10 ? '0' + h : h
+        let min = m < 10 ? '0' + m : m
+        let sec = s < 10 ? '0' + s : s
+
+        return <>{hour}:{min}:{sec}</>
+    }
+
+    const data = () => {
+        let date = d.getDate()
+        let mn = d.getMonth() + 1
+        let y = d.getFullYear()
+
+        let day = date < 10 ? '0' + date : date
+        let month = mn < 10 ? '0' + mn : mn
+        return <>{day}:{month}:{y}</>
+    }
+
+    const stringTime = time() // fix with date
+    const stringDate = data() // fix with date
 
     return (
         <div>
